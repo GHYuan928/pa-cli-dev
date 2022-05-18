82f1876a7767ce6f07b770c78c79f6829b4b4919
袁国 <yuanguo@dongfangfuli.com>
2022-05-19 00:22:14 +0800

    bin

---
+++ core/cli/bin/index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+const importlocal = require("import-local");
+// 如果传入 __filename 代表加载本地脚手架文件
+if (importlocal(__filename)) {
+  require("npmlog").info("cli", "正在使用 pa-cli-dev 本地版本");
+} else {
+  require("../lib")(process.argv(2));
+}
--- packages/core/bin/index.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node
-console.log("hello @pa-cli-dev/core hello ");
